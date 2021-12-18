29604e118e5ac1fda69217e001000bc0cbc85219
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@pop-os.localdomain>
2021-12-18 10:36:50 +0700

    removing contracts_directory

--- truffle-config.js
+++ truffle-config.js
@@ -4,7 +4,6 @@ const mnemonic = fs.readFileSync(".secret").toString().trim();
 require("dotenv").config();
 
 module.exports = {
-  contracts_build_directory: "../src/contracts",
   plugins: ["truffle-plugin-verify"],
   api_keys: {
     etherscan: process.env.ETHERSCAN_API_KEY,
